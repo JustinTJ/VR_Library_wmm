5cd2de4328759846457665a62ca5ca0c7afecbbf
JustinTJ <953351559@qq.com>
2024-08-05 03:07:24 +0800

    实现按钮点击图片 ui返回键 视频播放(bug)

---
+++ Assets/Scripts/ReplacePrefabOnClick.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReplacePrefabOnClick : MonoBehaviour
+{
+    public Button button; // 引用按钮
+    public GameObject prefabB; // 要生成的新预制体
+
+    void Start()
+    {
+        // 添加监听器到按钮
+        button.onClick.AddListener(ReplacePrefab);
+    }
+
+    void ReplacePrefab()
+    {
+        // 获取当前按钮所在的预制体的引用
+        GameObject currentPrefab = button.gameObject.transform.parent.gameObject;
+
+        // 获取当前预制体的父对象
+        Transform parentTransform = currentPrefab.transform.parent;
+
+        // 在销毁前获取按钮的父对象的索引
+        int siblingIndex = currentPrefab.transform.GetSiblingIndex();
+
+        // 销毁当前预制体
+        Destroy(currentPrefab);
+
+        // 实例化新的预制体并将其放置在原父对象中
+        GameObject newPrefab = Instantiate(prefabB, parentTransform);
+
+        // 设置新的预制体的顺序以匹配原来的预制体
+        newPrefab.transform.SetSiblingIndex(siblingIndex);
+    }
+}
